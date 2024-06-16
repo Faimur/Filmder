d7d64347392722094e4931272130411783016cf4
sooonaaa <sofya.kochergina.2016@mail.ru>
2024-06-17 01:42:04 +0300

    +18 фильмов

---
+++ zapolneniebd/SQLQuery1.sql
@@ -0,0 +1,37 @@
+INSERT INTO Tags (Name)
+VALUES
+('anarchy'),
+('mafia'),
+('virtual reality'),
+('vietnam war'),
+('disaster'),
+('antiquity'),
+('dinosaurs'),
+('cyberpunk');
+
+
+INSERT INTO Directors (Name)
+VALUES 
+('The Wachowskis'),
+('Jonathan Demme');
+
+INSERT INTO  Movies (Name, Description, TagID, GenreID, Picture, DirectorID, CountryID)
+VALUES 
+('Fight Club', 'An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.', '34', '8', 'C:\Users\Test\Desktop\Pictures\image6.jpg', '5', '147'),
+('The Godfather', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', '35', '6', 'C:\Users\Test\Desktop\Pictures\image7.jpg',  '8', '147'),
+('Goodfellas', 'The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.', '35', '4', 'C:\Users\Test\Desktop\Pictures\image8.jpg', '3', '147'),
+('The Lord of the Rings: The Fellowship of the Ring', 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.', '8', '2', 'C:\Users\Test\Desktop\Pictures\image9.jpg', '11', '116'),
+('The Matrix', 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', '36', '16','C:\Users\Test\Desktop\Pictures\image10.jpg',  '72', '147'),
+('Forrest Gump', 'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.', '37', '8', 'C:\Users\Test\Desktop\Pictures\image11.jpg', '39', '147'),
+('The Silence of the Lambs', 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.', '30', '17','C:\Users\Test\Desktop\Pictures\image12.jpg', '73', '147'),
+('Inglourious Basterds', 'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner''s vengeful plans for the same.', '1', '18', 'C:\Users\Test\Desktop\Pictures\image13.jpg', '2', '147'),
+('Interstellar', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.', '10', '16', 'C:\Users\Test\Desktop\Pictures\image14.jpg', '1', '147'),
+('Blade Runner', 'A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.', '41', '16', 'C:\Users\Test\Desktop\Pictures\image15.jpg', '12', '147'),
+('The Departed', 'An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in Boston.', '11', '6', 'C:\Users\Test\Desktop\Pictures\image16.jpg', '3', '147'),
+('Avatar', 'A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.', '8', '16', 'C:\Users\Test\Desktop\Pictures\image17.jpg', '10', '147'),
+('Titanic', 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.', '38', '15', 'C:\Users\Test\Desktop\Pictures\image18.jpg', '10', '147'),
+('The Grand Budapest Hotel', 'The adventures of Gustave H, a legendary concierge at a famous European hotel between the wars, and Zero Moustafa, the lobby boy who becomes his most trusted friend.', '12', '5', 'C:\Users\Test\Desktop\Pictures\image19.jpg', '13', '147'),
+('Gladiator', 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', '39', '1', 'C:\Users\Test\Desktop\Pictures\image20.jpg', '12', '147'),
+('The Big Lebowski', 'Jeff "The Dude" Lebowski, mistaken for a millionaire of the same name, seeks restitution for his ruined rug and enlists his bowling buddies to help get it.', '12', '5', 'C:\Users\Test\Desktop\Pictures\image21.jpg', '22', '147'),
+('The Social Network', 'As Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, he is sued by the twins who claimed he stole their idea, and by the co-founder who was later squeezed out of the business.', '14', '8', 'C:\Users\Test\Desktop\Pictures\image22.jpg', '5', '147'),
+('Jurassic Park', 'During a preview tour, a theme park suffers a major power breakdown that allows its cloned dinosaur exhibits to run amok.', '40', '16', 'C:\Users\Test\Desktop\Pictures\image23.jpg', '4', '147');
