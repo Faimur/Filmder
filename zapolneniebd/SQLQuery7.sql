f6c067ca28c17e342b1097a17079decfffa2f861
sooonaaa <sofya.kochergina.2016@mail.ru>
2024-06-16 19:36:22 +0300

    Пожалуйста, прочитайте сначала блокнотовский файлик

---
+++ zapolneniebd/SQLQuery7.sql
@@ -0,0 +1,80 @@
+﻿INSERT INTO Users (Login, Password)
+VALUES 
+('alpha1', 'ZxYvUt1#'), ('beta2', 'PwOmNf2@'), ('gamma3', 'YbNlRt3&'), ('delta4', 'MdCrXe4$'), ('epsilon5', 'HpJkLu5!'), ('zeta6', 'UoSiQp6%'),
+('eta7', 'VbTrNx7^'), ('theta8', 'WmApJf8*'), ('iota9', 'XeZjGt9('), ('kappa10', 'RnCdUv10)'), ('lambda11', 'TyPnXs11!'), ('mu12', 'EqFtGp12@'),
+('nu13', 'KhLwMd13#'), ('xi14', 'VoYnXq14$'), ('omicron15', 'TiLpUk15%'), ('pi16', 'WhDqNr16^'), ('rho17', 'XpBcMe17&'), ('sigma18', 'NlKrVt18*'),
+('tau19', 'UyTpJh19('), ('upsilon20', 'OkMnRx20)'), ('phi21', 'EpWtSf21!'), ('chi22', 'RbLvJu22@'), ('psi23', 'VkYpNt23#'), ('omega24', 'XjZcWo24$'),
+('alpha25', 'LmQrUs25%'), ('beta26', 'WbPkNx26^'), ('gamma27', 'OjDcYt27&'), ('delta28', 'KqFtPv28*'), ('epsilon29', 'LrNxWo29('), ('zeta30', 'SbXpMy30)'),
+('eta31', 'YdUrNt31!'), ('theta32', 'VjPlQo32@'), ('iota33', 'QwDtMf33#'), ('kappa34', 'NfYrUx34$'), ('lambda35', 'BmXpTv35%'), ('mu36', 'RkLtWz36^'),
+('nu37', 'CpQjNv37&'), ('xi38', 'ZlXwYs38*'), ('omicron39', 'DpKoJf39('), ('pi40', 'VrLxMq40)'), ('rho41', 'TnCrUx41!'), ('sigma42', 'PwQkMt42@'),
+('tau43', 'RlNyJs43#'), ('upsilon44', 'WvPtLf44$'), ('phi45', 'QjXtUr45%'), ('chi46', 'BkYrWo46^'), ('psi47', 'MdNpVs47&'), ('omega48', 'LjZtXq48*'),
+('alpha49', 'VrPoNu49('), ('beta50', 'XkUwYf50)'), ('gamma51', 'QpMtJs51!'), ('delta52', 'YnKrZt52@'), ('epsilon53', 'RwNpLf53#'), ('zeta54', 'SjVpXo54$'),
+('eta55', 'BkYrJd55%'), ('theta56', 'LpQtMw56^'), ('iota57', 'WnZxUs57&'), ('kappa58', 'RjVoLt58*'), ('lambda59', 'MqYpKf59('), ('mu60', 'XkNpTr60)'),
+('nu61', 'SjUwLr61!'), ('xi62', 'VkXpQm62@'), ('omicron63', 'TpRnJf63#'), ('pi64', 'WlKqUv64$'), ('rho65', 'BmYrXo65%'), ('sigma66', 'QkVpNt66^'),
+('tau67', 'XpRwLf67&'), ('upsilon68', 'NjPoZt68*'), ('phi69', 'VwLxUr69('), ('chi70', 'QpMtKs70)'), ('psi71', 'RnYrXj71!'), ('omega72', 'WvKpJf72@'),
+('alpha73', 'TpLxNs73#'), ('beta74', 'ZjQrWo74$'), ('gamma75', 'RkMtYf75%'), ('delta76', 'VpNxJd76^'), ('epsilon77', 'LqUwTr77&'), ('zeta78', 'WnXpMs78*'),
+('eta79', 'RjLkVf79('), ('theta80', 'XpKqUt80)'), ('iota81', 'QnWrJs81!'), ('kappa82', 'VkZpLt82@'), ('lambda83', 'TpNxWo83#'), ('mu84', 'LjQrYf84$'),
+('nu85', 'WkVxUd85%'), ('xi86', 'RpNtJs86^'), ('omicron87', 'ZlPwXo87&'), ('pi88', 'RjVkLf88*'), ('rho89', 'XpMtQr89('), ('sigma90', 'WnLxUs90)'),
+('tau91', 'VqNpKt91!'), ('upsilon92', 'TpYrXf92@'), ('phi93', 'RkUwJs93#'), ('chi94', 'LxMtVo94$'), ('psi95', 'QnWpZt95%'), ('omega96', 'WvKrLf96^'),
+('alpha97', 'TpLxMs97&'), ('beta98', 'VkQrYo98*'), ('gamma99', 'QjMtXo99('), ('delta100', 'RnXpLf100)');
+
+INSERT INTO Tags (Name)
+VALUES 
+('action'), ('comedy'), ('drama'), ('thriller'), ('horror'), ('romance'), ('sciencefiction'), ('fantasy'), ('animation'), ('adventure'),
+('mystery'), ('crime'), ('documentary'), ('biography'), ('family'), ('musical'), ('war'), ('history'), ('western'), ('sport'), ('superhero'),
+('indie'), ('noir'), ('cultclassic'), ('epic'), ('shortfilm'), ('silentfilm'), ('foreignfilm'), ('romcom'), ('psychological'), ('dreams'),
+('prison'), ('holocaust');
+
+INSERT INTO Genres (Name)
+VALUES 
+('action'), ('adventure'), ('animation'), ('biography'), ('comedy'), ('crime'), ('documentary'), ('drama'), ('family'), ('fantasy'),
+('history'), ('horror'), ('musical'), ('mystery'), ('romance'), ('science fiction'), ('thriller'), ('war'), ('western'), ('superhero'),
+('indie'), ('noir'), ('cult classic'), ('epic'), ('short film'), ('silent film'), ('foreign film'), ('romantic comedy'), ('psychological'),
+('sports'), ('historical fiction'), ('post-apocalyptic'), ('steampunk'), ('mockumentary'), ('slasher'), ('black comedy'), ('legal drama'),
+('political thriller'), ('space opera'), ('disaster'), ('spy'), ('road movie'), ('dance'), ('fantasy comedy'), ('dark fantasy'),
+('urban fantasy'), ('science fantasy'), ('parody'), ('satire'), ('heist');
+
+INSERT INTO Countries (Name)
+VALUES 
+('Australia'), ('Austria'), ('Azerbaijan'), ('Albania'), ('Algeria'), ('Angola'), ('Andorra'), ('Argentina'), ('Armenia'), ('Afghanistan'),
+('Bahamas'), ('Bangladesh'), ('Barbados'), ('Bahrain'), ('Belarus'), ('Belize'), ('Belgium'), ('Benin'), ('Bulgaria'), ('Bolivia'), ('Bosnia and Herzegovina'),
+('Botswana'), ('Brazil'), ('Brunei'), ('Burkina Faso'), ('Burundi'), ('Bhutan'), ('Vanuatu'), ('Vatican'), ('United Kingdom'), ('Hungary'),
+('Venezuela'), ('Vietnam'), ('Gabon'), ('Haiti'), ('Guyana'), ('Gambia'), ('Ghana'), ('Guatemala'), ('Guinea'), ('Germany'), ('Honduras'),
+('Grenada'), ('Greece'), ('Georgia'), ('Denmark'), ('Djibouti'), ('Dominica'), ('DR Congo'), ('Egypt'), ('Zambia'), ('Zimbabwe'), ('Israel'),
+('India'), ('Indonesia'), ('Jordan'), ('Iraq'), ('Iran'), ('Ireland'), ('Iceland'), ('Spain'), ('Italy'), ('Yemen'), ('Cape Verde'), ('Kazakhstan'),
+('Cambodia'), ('Cameroon'), ('Canada'), ('Qatar'), ('Kenya'), ('Cyprus'), ('Kyrgyzstan'), ('Kiribati'), ('China'), ('Colombia'), ('Comoros'),
+('Congo'), ('Costa Rica'), ('Côte d\''Ivoire'), ('Cuba'), ('Kuwait'), ('Laos'), ('Latvia'), ('Lesotho'), ('Liberia'), ('Lebanon'), ('Libya'),
+('Lithuania'), ('Liechtenstein'), ('Luxembourg'), ('Mauritius'), ('Mauritania'), ('Madagascar'), ('Macedonia'), ('Malawi'), ('Malaysia'),
+('Mali'), ('Maldives'), ('Malta'), ('Morocco'), ('Marshall Islands'), ('Mexico'), ('Micronesia'), ('Mozambique'), ('Moldova'), ('Monaco'),
+('Mongolia'), ('Myanmar'), ('Namibia'), ('Nauru'), ('Nepal'), ('Niger'), ('Nigeria'), ('Netherlands'), ('Nicaragua'), ('New Zealand'),('Norway'),
+('UAE'), ('Oman'), ('Pakistan'), ('Palau'), ('Panama'), ('Papua New Guinea'), ('Paraguay'), ('Peru'), ('Poland'), ('Portugal'), ('Russia'),
+('Rwanda'), ('Romania'), ('El Salvador'), ('Samoa'), ('San Marino'), ('Sao Tome and Principe'), ('Saudi Arabia'), ('Swaziland'), ('Seychelles'),
+('Senegal'), ('Saint Vincent and the Grenadines'), ('Saint Kitts and Nevis'), ('Saint Lucia'), ('Serbia'), ('Singapore'), ('Syria'), ('Slovakia'),
+('Slovenia'), ('USA'), ('Solomon Islands'), ('Somalia'), ('Sudan'), ('Suriname'), ('Sierra Leone'), ('Tajikistan'), ('Thailand'), ('Tanzania'),
+('Togo'), ('Tonga'), ('Trinidad and Tobago'), ('Tuvalu'), ('Tunisia'), ('Turkmenistan'), ('Turkey'), ('Uganda'), ('Uzbekistan'), ('Ukraine'),
+('Uruguay'), ('Fiji'), ('Philippines'), ('Finland'), ('France'), ('Croatia'), ('Central African Republic'), ('Chad'), ('Montenegro'), ('Czech Republic'),
+('Chile'), ('Switzerland'), ('Sweden'), ('Sri Lanka'), ('Ecuador'), ('Equatorial Guinea'), ('Eritrea'), ('Eswatini'), ('Estonia'),('Ethiopia'),
+('South Korea'), ('South Africa'), ('Jamaica'), ('Japan');
+
+INSERT INTO Directors (Name)
+VALUES 
+('Christopher Nolan'), ('Quentin Tarantino'), ('Martin Scorsese'), ('Steven Spielberg'), ('David Fincher'), ('Alfred Hitchcock'),
+('Stanley Kubrick'), ('Francis Ford Coppola'), ('Clint Eastwood'), ('James Cameron'), ('Peter Jackson'), ('Ridley Scott'), ('Wes Anderson'),
+('Greta Gerwig'), ('Denis Villeneuve'), ('Taika Waititi'), ('Spike Lee'), ('Bong Joon-ho'), ('Alejandro González Iñárritu'), ('Jordan Peele'),
+('Tim Burton'), ('Coen Brothers'), ('Woody Allen'), ('Sofia Coppola'), ('David Lynch'), ('Pedro Almodóvar'), ('Darren Aronofsky'),
+('Paul Thomas Anderson'), ('Guillermo del Toro'), ('Michael Bay'), ('Zack Snyder'), ('Ang Lee'), ('Kathryn Bigelow'), ('J.J. Abrams'),
+('Ron Howard'), ('Guy Ritchie'), ('John Woo'), ('Luc Besson'), ('Robert Zemeckis'), ('Spike Jonze'), ('Roman Polanski'), ('John Carpenter'),
+('Terrence Malick'), ('Lars von Trier'), ('Michael Mann'), ('George Lucas'), ('Oliver Stone'), ('Richard Linklater'), ('Baz Luhrmann'),
+('John Hughes'), ('Gus Van Sant'), ('Rob Reiner'), ('Mel Brooks'), ('François Truffaut'), ('Jean-Luc Godard'), ('Akira Kurosawa'),
+('Hayao Miyazaki'), ('Satyajit Ray'), ('Steven Soderbergh'), ('Danny Boyle'), ('Ken Loach'), ('Abbas Kiarostami'), ('Wong Kar-wai'),
+('Park Chan-wook'), ('David O. Russell'), ('Christopher McQuarrie'), ('M. Night Shyamalan'), ('Gaspar Noé'), ('Yorgos Lanthimos'),
+('Damien Chazelle'), ('Frank Darabont');
+
+INSERT INTO  Movies (Name, Description, TagID, GenreID, Picture, DirectorID, CountryID)
+VALUES 
+('Inception', 'A skilled thief enters people''s dreams to steal their secrets in this mind-bending thriller.', '31' , '16' ,'C:\Users\Test\Desktop\Pictures\image1.jpg', '1', '147'),
+('Pulp Fiction', 'Several interconnected stories of crime in Los Angeles intertwine in unexpected ways.', '12', '8','C:\Users\Test\Desktop\Pictures\image2.jpg', '2', '147'),
+('The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', '32', '8','C:\Users\Test\Desktop\Pictures\image3.jpg', '71', '147'),
+('The Dark Knight', 'When the menace known as The Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', '21', '1','C:\Users\Test\Desktop\Pictures\image4.jpg', '1', '147'),
+('Schindler''s List', 'In German-occupied Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.', '33', '4','C:\Users\Test\Desktop\Pictures\image5.jpg', '4', '147');
+
